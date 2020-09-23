5dc9b316b076d264c6c53b2beb8b7e44f161b6f3
Hannah Ripley <ripleyhk@stu.cs.jmu.edu>
2020-09-23 11:27:45 -0400

    Improved lost.html

--- scripts/home.js
+++ scripts/home.js
@@ -4,6 +4,6 @@ let choice = document.getElementById("stay");
 
 choice.addEventListener("click", function() {
   audio.play();
-  document.getElementsByTagName("body")[0].classList.add('night');
+  document.getElementsByClassName("curtain")[0].classList.toggle('open-curtain');
   document.getElementById("stay").classList.add('clear');
 } );
